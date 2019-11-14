2c362e03bb89b64f67bdb92cfe896013ded35447
MgBacon <mgcbkmn@googlemail.com>
2019-11-15 00:14:09 +0100

    Updated UploadUi
    - added more error handling, giving the user tiny bit more if uploading fails
    - only shows button if upload is successful

--- app/src/main/java/uk/cg0/okuru/UploadActivity.kt
+++ app/src/main/java/uk/cg0/okuru/UploadActivity.kt
@@ -1,20 +1,18 @@
 package uk.cg0.okuru
 
-import android.content.ClipboardManager
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
 import kotlinx.android.synthetic.main.activity_upload.*
 import android.text.method.ScrollingMovementMethod
-import android.os.StrictMode
 import android.provider.MediaStore
-import android.view.View
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.core.BlobDataPart
+import android.os.AsyncTask
+import java.net.MalformedURLException
+import android.view.View
 import org.json.JSONObject
-import android.content.ClipData
-import android.widget.Toast
+
 
 class UploadActivity : AppCompatActivity() {
 
@@ -24,48 +22,72 @@ class UploadActivity : AppCompatActivity() {
 
         when {
             intent?.action == Intent.ACTION_SEND -> {
-                handleSendImage(intent)
+                AsyncTask.execute {
+                    handleSendImage(intent)
+                }
             }
             else -> {
                 // Handle other intents, such as being started from the home screen
             }
         }
-        paste_button.setOnClickListener(View.OnClickListener {
-            val myClipboard: ClipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-            val myClip: ClipData = ClipData.newPlainText("text", upload.text)
+        /*paste_button.setOnClickListener {
+            val myClipboard: ClipboardManager =
+                getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val myClip: ClipData = ClipData.newPlainText("upload link", upload.text)
             myClipboard.setPrimaryClip(myClip)
             Toast.makeText(this, "Link copied", Toast.LENGTH_LONG).show()
-        })
-
+        }
+        */
     }
 
     private fun handleSendImage(intent: Intent) {
         //TODO: move to a seperate thread
-        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        /*val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
         StrictMode.setThreadPolicy(policy)
+         */
         upload.text = intent.clipData?.getItemAt(0)?.uri.toString()
         imageView.setImageURI(intent.clipData?.getItemAt(0)?.uri)
         upload.movementMethod = ScrollingMovementMethod()
         val inputStream = contentResolver.openInputStream(intent.clipData?.getItemAt(0)?.uri!!)
         val projection = arrayOf(MediaStore.MediaColumns.DISPLAY_NAME)
-        val metaCursor = contentResolver.query(intent.clipData?.getItemAt(0)?.uri!!, projection, null, null, null)
-        var filename=""
-
+        val metaCursor = contentResolver.query(
+            intent.clipData?.getItemAt(0)?.uri!!,
+            projection,
+            null,
+            null,
+            null
+        )
+        var filename = ""
         if (metaCursor != null) {
             try {
                 if (metaCursor!!.moveToFirst()) {
                     filename = metaCursor!!.getString(0)
+                    filename_text.text = filename
                 }
             } finally {
                 metaCursor!!.close()
             }
         }
-        filename_text.text=filename
-        val (_,response, _)= Fuel.upload("endpoint")
-            .add(BlobDataPart(inputStream!!, name="files[]",filename = filename))
-            .header("token", "token")
-            .response()
-        upload.text =JSONObject(String(response.data)).getJSONArray("files").getJSONObject(0).getString("url").replace("fucking", "uwu")
+
+        try {
+            val (_, response, _) = Fuel.upload("endpoint")
+                .add(BlobDataPart(inputStream!!, name = "files[]", filename = filename))
+                .header("token", "token")
+                .response()
+            if (response.statusCode == 200) {
+                    upload.text =
+                    JSONObject(String(response.data)).getJSONArray("files").getJSONObject(0).getString("url")
+                    runOnUiThread {paste_button.visibility = View.VISIBLE}
+            } else {
+                upload.text = String.format(
+                    "%s: %s",
+                    response.statusCode.toString(),
+                    response.responseMessage
+                )
+            }
+        } catch (mue: MalformedURLException) {
+            upload.text = "Invalid upload URL"
+        }
     }
 }
 
