4d64647b3385b7d81ec7ece1b7e668b73bd48c05
MgBacon <mgcbkmn@googlemail.com>
2019-12-26 05:54:22 +0100

    Fixed progress calculations + added 404 info
    - Fixed progress calculations to be "XX%"
    - Add additional response to a 404 reponse adding more info about faulty config

--- app/src/main/java/uk/cg0/okuru/UploadActivity.kt
+++ app/src/main/java/uk/cg0/okuru/UploadActivity.kt
@@ -1,11 +1,13 @@
 package uk.cg0.okuru
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
 import kotlinx.android.synthetic.main.activity_upload.*
 import android.text.method.ScrollingMovementMethod
-import android.provider.MediaStore
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.core.BlobDataPart
 import android.os.AsyncTask
@@ -13,6 +15,8 @@ import java.net.MalformedURLException
 import android.view.View
 import org.json.JSONObject
 import android.provider.OpenableColumns
+import android.widget.Toast
+import kotlin.math.roundToInt
 
 
 class UploadActivity : AppCompatActivity() {
@@ -31,14 +35,12 @@ class UploadActivity : AppCompatActivity() {
                 // Handle other intents, such as being started from the home screen
             }
         }
-        /*paste_button.setOnClickListener {
-            val myClipboard: ClipboardManager =
-                getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        paste_button.setOnClickListener {
+            val myClipboard: ClipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
             val myClip: ClipData = ClipData.newPlainText("upload link", upload.text)
             myClipboard.setPrimaryClip(myClip)
             Toast.makeText(this, "Link copied", Toast.LENGTH_LONG).show()
         }
-        */
     }
 
     private fun handleSendImage(intent: Intent) {
@@ -49,7 +51,8 @@ class UploadActivity : AppCompatActivity() {
         upload.text = intent.clipData?.getItemAt(0)?.uri.toString()
         upload.movementMethod = ScrollingMovementMethod()
         val inputStream = contentResolver.openInputStream(intent.clipData?.getItemAt(0)?.uri!!)
-        var size:Long=0
+        var filesize= 0.0F
+        var filename= ""
         contentResolver.query(
             intent.clipData?.getItemAt(0)?.uri!!,
             null,
@@ -60,32 +63,37 @@ class UploadActivity : AppCompatActivity() {
                 val nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
                 val sizeIndex = cursor.getColumnIndex(OpenableColumns.SIZE)
                 cursor.moveToFirst()
-                filename_text.text = cursor.getString(nameIndex)
-                size=cursor.getLong(sizeIndex)
-                filesize_text.text =size.toString()
+                filename=cursor.getString(nameIndex)
+                filesize=cursor.getLong(sizeIndex).toFloat()
         }
 
         try {
-            val (_, response, _) = Fuel.upload("endpoint")
-                .add(BlobDataPart(inputStream!!, name = "files[]", filename = filename_text.text.toString()))
-                .header("token", "token")
+            val (_, response, _) = Fuel.upload(getString(R.string.URL))
+                .add(BlobDataPart(inputStream!!, name = "files[]", filename = filename))
+                .header("token", getString(R.string.SAFE_TOKEN))
                 .requestProgress{ readBytes,_ ->
-                    if(readBytes<size){
-                        val progress=(readBytes/size)*100
+                    if(readBytes<filesize){
+                        val progress=((readBytes/filesize)*100).toInt()
                         upload.text="Upload progress: $progress%"
                     }
                 }
                 .response()
-            if (response.statusCode == 200) {
+            when(response.statusCode) {
+                200 -> {
                     upload.text = JSONObject(String(response.data)).getJSONArray("files").getJSONObject(0).getString("url")
                     runOnUiThread {
                         imageView.setImageURI(intent.clipData?.getItemAt(0)?.uri)
                         paste_button.visibility = View.VISIBLE
                     }
-            } else {
-                val code=response.statusCode.toString()
-                val message=response.responseMessage
-                upload.text = "$code: $message"
+                }
+                404 -> {
+                    upload.text="Upload API can't be reached, sure URL is correct?"
+                }
+                else -> {
+                    val code = response.statusCode.toString()
+                    val message = response.responseMessage
+                    upload.text = "$code: $message"
+                }
             }
         } catch (mue: MalformedURLException) {
             upload.text = "Invalid upload URL"
